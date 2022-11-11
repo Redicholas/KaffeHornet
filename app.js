43d6fad49dd47d9cbfd9ab970fa4a17bf16deecc
Nicholas Peters <peters.nicholas@gmail.com>
2022-11-11 08:46:40 +0100

    :truck: Produkter in i javascripten

--- app.js
+++ app.js
@@ -1,1 +1,74 @@
-console.log("Hello World!")
+const products = [
+    {
+        "product-id": 1,
+        "name":  "Sibaristica",
+        "desc": "Bönor från Guatemala och Colombia",
+        "price": 129,
+        "amount": 0
+    },
+    {
+        "product-id": 2,
+        "name":  "Blue Magic",
+        "desc": "Espressobönor",
+        "price": 119,
+        "amount": 0
+    },
+    {
+        "product-id": 3,
+        "name":  "Specialty Beans",
+        "desc": '"Speciella" bönor från Ethiopien',
+        "price": 129,
+        "amount": 0
+    },
+    {
+        "product-id": 4,
+        "name":  "Rustik",
+        "desc": "Svart kopp i keramik",
+        "price": 49,
+        "amount": 0
+    },
+    {
+        "product-id": 5,
+        "name":  "Grön",
+        "desc": "Kopp i poppande grön färg",
+        "price": 39,
+        "amount": 0
+    },
+    {
+        "product-id": 6,
+        "name":  "V60",
+        "desc": "V60 bryggaren som tilltalar din inre hipster",
+        "price": 599,
+        "amount": 0
+    },
+    {
+        "product-id": 7,
+        "name":  "Aeropress",
+        "desc": "För dig som vill kunna göra kaffe var som helst!",
+        "price": 499,
+        "amount": 0
+    },
+    {
+        "product-id": 8,
+        "name":  "Fancy",
+        "desc": "För ditt finbesök",
+        "price": 49,
+        "amount": 0
+    },
+    {
+        "product-id": 9,
+        "name":  "Franskpress",
+        "desc": "Den klassiska Franskpressen går alltid hem",
+        "price": 199,
+        "amount": 0
+    },
+    {
+        "product-id": 10,
+        "name":  "Orange",
+        "desc": "En större kopp för dig med ett större beroende",
+        "price": 39,
+        "amount": 0
+    },
+]
+
+console.log(products[6].desc)
