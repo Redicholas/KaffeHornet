4acdfebfcef58d4f63dc2eebce8cb002e31b9b18
grapensparr <liza.grapensparr@gmail.com>
2022-11-13 21:28:50 +0100

    Quantity

---
+++ quantity.js
@@ -0,0 +1,141 @@
+
+function click1(click) {
+    const quantity1 = document.getElementById("quantity1");
+    const sumvalue = parseInt(quantity1.innerText) + click;
+    quantity1.innerText = sumvalue;
+  
+    if(sumvalue <0) {
+      quantity1.innerText = 0;
+    }
+  
+    if(click === 0) {
+      quantity1.innerText = 0;
+    }
+  }
+  
+  function click2(click) {
+    const quantity2 = document.getElementById("quantity2");
+    const sumvalue = parseInt(quantity2.innerText) + click;
+    quantity2.innerText = sumvalue;
+  
+    if(sumvalue <0) {
+      quantity2.innerText = 0;
+    }
+  
+    if(click === 0) {
+      quantity2.innerText = 0;
+    }
+  }
+  
+  function click3(click) {
+    const quantity3 = document.getElementById("quantity3");
+    const sumvalue = parseInt(quantity3.innerText) + click;
+    quantity3.innerText = sumvalue;
+  
+    if(sumvalue <0) {
+      quantity3.innerText = 0;
+    }
+  
+    if(click === 0) {
+      quantity3.innerText = 0;
+    }
+  }
+  
+  function click4(click) {
+    const quantity4 = document.getElementById("quantity4");
+    const sumvalue = parseInt(quantity4.innerText) + click;
+    quantity4.innerText = sumvalue;
+  
+    if(sumvalue <0) {
+      quantity4.innerText = 0;
+    }
+  
+    if(click === 0) {
+      quantity4.innerText = 0;
+    }
+  }
+  
+  function click5(click) {
+    const quantity5 = document.getElementById("quantity5");
+    const sumvalue = parseInt(quantity5.innerText) + click;
+    quantity5.innerText = sumvalue;
+  
+    if(sumvalue <0) {
+      quantity5.innerText = 0;
+    }
+  
+    if(click === 0) {
+      quantity5.innerText = 0;
+    }
+  }
+  
+  function click6(click) {
+    const quantity6 = document.getElementById("quantity6");
+    const sumvalue = parseInt(quantity6.innerText) + click;
+    quantity6.innerText = sumvalue;
+  
+    if(sumvalue <0) {
+      quantity6.innerText = 0;
+    }
+  
+    if(click === 0) {
+      quantity6.innerText = 0;
+    }
+  }
+  
+  function click7(click) {
+    const quantity7 = document.getElementById("quantity7");
+    const sumvalue = parseInt(quantity7.innerText) + click;
+    quantity7.innerText = sumvalue;
+  
+    if(sumvalue <0) {
+      quantity7.innerText = 0;
+    }
+  
+    if(click === 0) {
+      quantity7.innerText = 0;
+    }
+  }
+  
+  function click8(click) {
+    const quantity8 = document.getElementById("quantity8");
+    const sumvalue = parseInt(quantity8.innerText) + click;
+    quantity8.innerText = sumvalue;
+  
+    if(sumvalue <0) {
+      quantity8.innerText = 0;
+    }
+  
+    if(click === 0) {
+      quantity8.innerText = 0;
+    }
+  }
+  
+  
+  function click9(click) {
+    const quantity9 = document.getElementById("quantity9");
+    const sumvalue = parseInt(quantity9.innerText) + click;
+    quantity9.innerText = sumvalue;
+  
+    if(sumvalue <0) {
+      quantity9.innerText = 0;
+    }
+  
+    if(click === 0) {
+      quantity9.innerText = 0;
+    }
+  }
+
+  function click10(click) {
+    const quantity10 = document.getElementById("quantity10");
+    const sumvalue = parseInt(quantity10.innerText) + click;
+    quantity10.innerText = sumvalue;
+
+    if(sumvalue <0) {
+      quantity10.innerText = 0;
+    }
+  
+    if(click === 0) {
+      quantity10.innerText = 0;
+    }
+  }
