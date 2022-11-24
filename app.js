a29f25046605c9cae0d959b021a9591458d786a9
Nicholas Peters <peters.nicholas@gmail.com>
2022-11-24 10:23:43 +0100

    Revert "Merge pull request #29 from Medieinstitutet/formulärsdelen-tittie"
    This reverts commit 24812af6ba9752f29cc7ae49c19f4c5200f7c434, reversing
    changes made to 42d1b82f87c7927e8f3dd36a666583d09be5d470.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-use-before-define */
 const products = [
   {
+    name: 'Sibaristica',
     img: ['img/produkt-1-kaffepasar.jpg', 'img/produkt-1-kaffepasar-mirror.jpg'],
     imgAlt: ['Två påsar med kaffebönor', 'Spegelvänd bild på två påsar med kaffebönor'],
     desc: 'Bönor från Guatemala och Colombia',
@@ -10,6 +11,7 @@ const products = [
     rating: rating4stars.innerHTML,
   },
   {
+    name: 'Blue Magic',
     img: ['img/produkt-2-andrakaffapasar.jpg', 'img/produkt-2-andrakaffapasar-mirror.jpg'],
     imgAlt: ['Kaffebönor i påse med blå etikett', 'Spegelvänd bild på kaffebönor i påse med blå etikett'],
     desc: 'Espressobönor',
@@ -19,6 +21,7 @@ const products = [
     rating: rating4stars.innerHTML,
   },
   {
+    name: 'Specialty Beans',
     img: ['img/produkt-3-merkaffe.jpg', 'img/produkt-3-merkaffe-mirror.jpg'],
     imgAlt: ['En arm som håller upp en påse kaffebönor', 'Spegelvänd bild på en arm som håller upp en påse kaffebönor'],
     desc: '"Speciella" bönor från Ethiopien',
@@ -28,6 +31,7 @@ const products = [
     rating: rating4stars.innerHTML,
   },
   {
+    name: 'Rustik',
     img: ['img/produkt-4-kopp.jpg', 'img/produkt-4-kopp-mirror.jpg'],
     imgAlt: ['En svart kaffekopp på ett träbord', 'Spegelvänd bild på en svart kaffekopp på ett träbord'],
     desc: 'Svart kopp i keramik',
@@ -37,6 +41,7 @@ const products = [
     rating: rating3stars.innerHTML,
   },
   {
+    name: 'Grön',
     img: ['img/produkt-5-kopp2.jpg', 'img/produkt-5-kopp2-mirror.jpg'],
     imgAlt: [
       'Grön kaffekopp med latteart, på ett vitt bort med kaffebönor strödda runt om',
@@ -49,6 +54,7 @@ const products = [
     rating: rating5stars.innerHTML,
   },
   {
+    name: 'V60',
     img: ['img/produkt-6-v60.jpg', 'img/produkt-6-v60-mirror.jpg'],
     imgAlt: [
       'V60 bryggare med kaffe som brygger i ett café',
@@ -61,6 +67,7 @@ const products = [
     rating: rating4stars.innerHTML,
   },
   {
+    name: 'Aeropress',
     img: ['img/produkt-7-aeropress.jpg', 'img/produkt-7-aeropress-mirror.jpg'],
     imgAlt: ['En aeropress på en stubbe utomhus', 'Spegelvänd bild på en aeropress på en stubbe utomhus'],
     desc: 'För dig som vill kunna göra kaffe var som helst!',
@@ -70,6 +77,7 @@ const products = [
     rating: rating3stars.innerHTML,
   },
   {
+    name: 'Fancy',
     img: ['img/produkt-8-kopp3.jpg', 'img/produkt-8-kopp3-mirror.jpg'],
     imgAlt: ['Liten kaffekopp med guld detaljer', 'Spegelvänd bild på liten kaffekopp med guld detaljer'],
     desc: 'För ditt finbesök',
@@ -79,6 +87,7 @@ const products = [
     rating: rating4stars.innerHTML,
   },
   {
+    name: 'Franskpress',
     img: ['img/produkt-9-franskpress.jpg', 'img/produkt-9-franskpress-mirror.jpg'],
     imgAlt: [
       'En franskpress med kaffe och en kaffekopp brevid',
@@ -91,6 +100,7 @@ const products = [
     rating: rating5stars.innerHTML,
   },
   {
+    name: 'Orange',
     img: ['img/produkt-10-kopp4.jpg', 'img/produkt-10-kopp4-mirror.jpg'],
     imgAlt: [
       'Hand som håller en stor orange kopp med rykande kaffe',
@@ -384,208 +394,120 @@ function byRating(a, b) {
   }
 }
 
-// *****************FORMULÄRVALIDERING STARTAR HÄR***************************************
+/* Fomulärsdelen  
 
-const nameError = document.getElementById("name-error");
-const lastNameError = document.getElementById("lastname-error");
-const adressError = document.getElementById("adress-error");
-const zipError = document.getElementById("zip-error");
-const cityError = document.getElementById("city-error");
-const phoneError = document.getElementById("phone-error");
-const emailError = document.getElementById("email-error");
-const personalNrError = document.getElementById('personalNumber-Error');
-const cardNrError = document.getElementById('cardNrError');
-const cvvError = document.getElementById('cvvError');
+[] Beställningsknappen ska inte vara klickbar 
+[] Kunden väljer betalsätt 
+[] Om kunden väljer faktura
+    Visas ett formulär med personnummer.
+      Kortforumuläret döljs 
+[] Om kunden väljer kort 
+    Visas ett formulär med kortuppgifter 
+    fakturaformuläret döljs
+[] kontrollera att alla fält är korrekt ifyllda 
+[] aktivera beställnings-knappen */
 
-const firstNameField = document.querySelector('#fname');
-const lastNameField = document.querySelector('#lname');
-const adressField = document.querySelector('#adress');
-const zipCodeField = document.querySelector('#zip');
-const cityField = document.querySelector('#city');
-const phoneField = document.querySelector('#phone');
-const emailField = document.querySelector('#email');
-const personalNrField = document.querySelector('#personalNumber');
-const cardNrField = document.querySelector('#cardNr');
-const cvvField = document.querySelector('#cvv');
-const orderBtn = document.getElementById('buy-button')
-
-let fNameIsOk = false;
-let lNameIsOk = false;
-let adressIsOk = false;
-let zipCodeIsOk = false;
-let cityIsOk = false;
-let phoneIsOk = false;
-let emailIsOk = false;
-let personalNrIsOk = false;
-let cardNrIsOk = false;
-let cvvIsOk = false;
-
-firstNameField.addEventListener('focusout', validateName);
-lastNameField.addEventListener('focusout', validateLastName);
-adressField.addEventListener('focusout', validateAdress);
-zipCodeField.addEventListener('focusout', validateZip);
-cityField.addEventListener('focusout', validateCity);
-phoneField.addEventListener('focusout', validatePhone);
-emailField.addEventListener('focusout', validateEmail);
-personalNrField.addEventListener('focusout', validatePersonalNumber);
-cardNrField.addEventListener('focusout', validateCardNr);
-cvvField.addEventListener('focusout', validateCvv);
-
-function validateCardNr() {
-  if(cardNrField.value.length === 0) {
-    cardNrIsOk = false;
-    cardNrError.innerHTML ="*"
-  }
-  if(cardNrField.value.length < 16 || cardNrField.value.length > 16) {
-    cardNrIsOk = false;
-    cardNrError.innerHTML ="16 siffror tack"
-  } else {
-  cardNrError.innerHTML = '<i class="fa-solid fa-check"></i>';
-  cardNrIsOk = true;
-  }
-  activateOrderButton();
-}
-
-function validateCvv() {
-  if(cvvField.value.length === 0) {
-    cvvIsOk = false;
-    cvvError.innerHTML = "*"
-  }
-  if(cvvField.value.length < 3 || cvvField.value.length > 3) {
-    cvvIsOk = false;
-    cvvError.innerHTML = "3 siffror tack"
-  } else {
-    cvvIsOk = true;
-    cvvError.innerHTML = '<i class="fa-solid fa-check"></i>';
-  }
-  activateOrderButton();
-}
+let nameError = document.getElementById('name-error');
+let lastNameError = document.getElementById('lastname-error');
+let adressError = document.getElementById('adress-error');
+let zipError = document.getElementById('zip-error');
+let cityError = document.getElementById('city-error');
+let phoneError = document.getElementById('phone-error');
+let emailError = document.getElementById('email-error');
 
 function validateName() {
-  if (firstNameField.value.length === 0) {
-    fNameIsOk = false;
-    nameError.innerHTML = "Måste fyllas i";
-  } else {
-  fNameIsOk = true;
-  nameError.innerHTML = '<i class="fa-solid fa-check"></i>';
+  let name = document.getElementById('fname').value;
+
+  if (name.length == -1) {
+    nameError.innerHTML = '*';
+    return false;
   }
-  activateOrderButton();
+  nameError.innerHTML = '<i class="fa-solid fa-check"></i>';
+  return true;
 }
 
 function validateLastName() {
-  if (lastNameField.value.length === 0) {
-    lNameIsOk = false;
-    lastNameError.innerHTML = "Måste fyllas i";
-  } else {
-    lNameIsOk = true;
-    lastNameError.innerHTML = '<i class="fa-solid fa-check"></i>';
+  let lastName = document.getElementById('lname').value;
+
+  if (lastName.length == -1) {
+    lastNameError.innerHTML = '*';
+    return false;
   }
-  activateOrderButton();
+  lastNameError.innerHTML = '<i class="fa-solid fa-check"></i>';
+  return true;
 }
 
 function validateAdress() {
-  if (adressField.value.length === 0) {
-    adressIsOk = false;
-    adressError.innerHTML = "Måste fyllas i";
-  } else {
-    adressIsOk = true;
-    adressError.innerHTML = '<i class="fa-solid fa-check"></i>';
+  let adress = document.getElementById('adress').value;
+
+  if (adress.length == -1) {
+    adressError.innerHTML = '*';
+    return false;
   }
-  activateOrderButton();
+  adressError.innerHTML = '<i class="fa-solid fa-check"></i>';
+  return true;
 }
 
 function validateZip() {
-  if (zipCodeField.value.length === 0) {
-    zipCodeIsOk = false;
-    zipError.innerHTML = "Måste fyllas i";
-  } else {
-    zipCodeIsOk = true;
-    zipError.innerHTML = '<i class="fa-solid fa-check"></i>';
+  let zipCode = document.getElementById('zip').value;
+
+  if (zipCode.length == -1) {
+    zipError.innerHTML = '*';
+    return false;
   }
-  activateOrderButton();
+  zipError.innerHTML = '<i class="fa-solid fa-check"></i>';
+  return true;
 }
 
 function validateCity() {
-  if (cityField.value.length === 0) {
-    cityIsOk = false;
-    cityError.innerHTML = "Måste fyllas i";
-  } else {
-    cityIsOk = true;
-    cityError.innerHTML = '<i class="fa-solid fa-check"></i>';
+  let city = document.getElementById('city').value;
+
+  if (city.length == -1) {
+    cityError.innerHTML = '*';
+    return false;
   }
-  activateOrderButton();
+  cityError.innerHTML = '<i class="fa-solid fa-check"></i>';
+  return true;
 }
 
 function validatePhone() {
-  if (phoneField.value.length === 0) {
-    phoneIsOk = false;
-    phoneError.innerHTML = "Måste fyllas i";
+  let phone = document.getElementById('phone').value;
+
+  if (phone.length == -1) {
+    phoneError.innerHTML = '*';
+    return false;
   }
-  if (!phoneField.value.match(/^[0-9]{10}$/)) {
-    phoneIsOk = false;
-    phoneError.innerHTML = "var god fyll i ett giltligt mobilnr";
-  }  else {
-    phoneIsOk = true;
-    phoneError.innerHTML = '<i class="fa-solid fa-check"></i>';
+
+  if (!phone.match(/^[0-9]{10}$/)) {
+    phoneError.innerHTML = 'Endast siffror';
+    return false;
   }
-  activateOrderButton();
+
+  phoneError.innerHTML = '<i class="fa-solid fa-check"></i>';
+  return true;
 }
 
 function validateEmail() {
-  if (emailField.value.length === 0) {
-    emailIsOk = false;
-    emailError.innerHTML = "Måste fyllas i";
+  let email = document.getElementById('email').value;
+
+  if (email.length == -1) {
+    emailError.innerHTML = '*';
+    return false;
   }
-  if (!emailField.value.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
-    emailIsOk = false;
-    emailError.innerHTML = "Ogiltlig mejl";
-  } else {
-    emailIsOk = true;
-    emailError.innerHTML = '<i class="fa-solid fa-check"></i>';
+
+  if (!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+    emailError.innerHTML = 'Ogiltlig mejl';
+    return false;
   }
-  activateOrderButton();
+  emailError.innerHTML = '<i class="fa-solid fa-check"></i>';
+  return true;
 }
 
-function validatePersonalNumber() {
-  if (personalNrField.value.length === 0) {
-    personalNrIsOk = false;
-    personalNrError.innerHTML = "*";
-  }
-  if (!personalNrField.value.match(/^(\d{6}|\d{8})[-|(\s)]{0,1}\d{4}$/)) {
-    personalNrIsOk = false;
-    personalNrError.innerHTML = "10 siffror tack";
-  } else {
-    personalNrIsOk = true;
-    personalNrError.innerHTML = '<i class="fa-solid fa-check"></i>'; 
-  }
-  activateOrderButton();
-}
+document.querySelector('#paymentType_invoice').addEventListener('click', handleInvoiceClick);
+document.querySelector('#paymentType_card').addEventListener('click', handleCardClick);
 
-// ****************************VALIDERING SLUT*******************************************
-
-
-
-// *********FÖR AKTIVERA BESTÄLLNINGSKNAPPEN NÄR VALIDERINGSVILLKOREN ÄR UPPFYLLDA***************
-
-function activateOrderButton(){
-  if (fNameIsOk && lNameIsOk && adressIsOk && zipCodeIsOk && cityIsOk && phoneIsOk && emailIsOk && ((personalNrIsOk) || (cardNrIsOk && cvvIsOk))){
-      orderBtn.removeAttribute('disabled');    
-  } else {
-      orderBtn.setAttribute('disabled', ''); 
-  }
-}
-
-
-document.querySelector('#paymentType_invoice').addEventListener('click', handleInvoiceClick); 
-document.querySelector('#paymentType_card').addEventListener('click',handleCardClick); 
-
-
-const card = document.getElementById('creditCard');
-const invoice = document.getElementById('invoice');
-
-// **************AKTIVERING BESTÄLLNINGSKNAPP SLUT******************************
-
-//***********FÖR ATT DÖLJA/VISA FORMULÄREN FÖR KORT OCH FAKTURA************
+const card = document.getElementById('id-tva');
+const invoice = document.getElementById('id-ett');
 
 function handleCardClick() {
   if (document.getElementById('paymentType_card').checked) {
@@ -603,28 +525,6 @@ function handleInvoiceClick() {
   } else {
     invoice.style.display = 'none';
   }
-}
-
-//**********RENSA FORMULÄR******************
-
-document.querySelector('#eraseBtn').addEventListener('click', resetOrder);
-
-function resetOrder() {
-  document.getElementById('form').reset();
-  document.getElementById('invoice').reset();
-  document.getElementById('creditCard').reset();
-
-  clearBasket();
-  validateCardNr();
-  validatePersonalNumber();
-  validateCvv();
-  validateName();
-  validateLastName();
-  validateAdress();
-  validateZip();
-  validateCity();
-  validatePhone();
-  validateEmail();
 }
 
 renderProducts();
