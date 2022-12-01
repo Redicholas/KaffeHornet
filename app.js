6268271fbafac664da11c61d6dd4401d20987a84
Nicholas Peters <peters.nicholas@gmail.com>
2022-12-01 11:57:33 +0100

    Snyggare knappar, lagad aeropressbild

--- app.js
+++ app.js
@@ -405,8 +405,8 @@ function renderProducts() {
                 alt="${sortedProducts[i].img[1].alt}"
               />
               <div class="btnHolder">
-                <button id="prevImg-${i}" class="prevImg imgBtn"> < </button>         
-                <button id="nextImg-${i}" class="nextImg imgBtn"> > </button>
+                <button id="prevImg-${i}" class="prevImg imgBtn"> <i class="fa-solid fa-chevron-left"></i> </button>         
+                <button id="nextImg-${i}" class="nextImg imgBtn"> <i class="fa-solid fa-chevron-right"></i> </button>
               </div>
           </div>
             <div class="product-info">
